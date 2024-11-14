e8c75dbff676529f6f8c35ddbedbb583735d2b6a
Marcel Niemiec <marcel.niemiec6@gmail.com>
2024-11-14 14:51:04 +0000

    Save progress: 950.1

--- bingo.sh
+++ bingo.sh
@@ -1,8 +1,13 @@
-# !/bin/bash
+#!/bin/bash
+
 # Bingo Number Generator
 
 echo -e "\n~~ Bingo Number Generator ~~\n"
 
-NUMBER=$((RANDOM%75+1))
+NUMBER=$(( RANDOM % 75 + 1 ))
 TEXT="The next number is, "
-echo $NUMBER
+
+if (( NUMBER <= 15 ))
+then
+  echo $TEXT B:$NUMBER
+fi
