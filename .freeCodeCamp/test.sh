1f3464df0da0c26e54f64dc44550e90d057eb922
Marcel Niemiec <marcel.niemiec6@gmail.com>
2024-11-14 15:32:15 +0000

    Save progress: 1412.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -6,9 +6,17 @@ RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again late
 N=$(( RANDOM % 6 ))
 
 function GET_FORTUNE() {
-  echo -e "Ask a yes or no question:\n"
+  if [[ ! $1 ]]
+  then
+    echo -e "Ask a yes or no question:\n"
+  else
+    echo "Try again. Make sure it ends with a question mark:"
+  fi
   read QUESTION
+  
 }
-
-GET_FORTUNE
-echo $QUESTION
+until [[ $QUESTION =~ \?$ ]]
+do
+  GET_FORTUNE
+done
+GET_FORTUNE again
