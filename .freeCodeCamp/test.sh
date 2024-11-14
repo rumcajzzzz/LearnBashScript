92be4ca4e38da74149d7efb4e64f7fbc911d83c9
Marcel Niemiec <marcel.niemiec6@gmail.com>
2024-11-14 15:09:16 +0000

    Save progress: 1190.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -3,5 +3,9 @@
 echo -e "\n~~ Fortune Teller ~~\n"
 
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
-N=$(( RANDOM %1 ))
-echo ${RESPONSES[$N]}
+N=$(( RANDOM % 6 ))
+
+function GET_FORTUNE() {
+  echo "Ask a yes or no question:"
+}
+GET_FORTUNE
